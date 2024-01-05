62a31341aff20f78e0b61e513bda0ea1fe8b1c09
miulerbm <miulerbm00@gmail.com>
2024-01-05 17:54:05 -0500

    Scroll con Load More Button

--- src/App.tsx
+++ src/App.tsx
@@ -1,32 +1,76 @@
-import { useEffect, useState } from "react";
+// import { useEffect, useState } from "react";
 import "./App.css";
 import TodoCard from "./components/TodoCard";
 import { todo } from "./types/todo";
+import { useInfiniteQuery } from "@tanstack/react-query";
 
 function App() {
-  const [todos, setTodos] = useState<todo[]>([]);
+  // const [todos, setTodos] = useState<todo[]>([]);
 
   // Creating a function for fetching random data
-  const fetchTodos = async () => {
+  const fetchTodos = async ({ pageParam }: { pageParam: number }) => {
     const res = await fetch(
-      "https://jsonplaceholder.typicode.com/todos?_page=1"
+      `https://jsonplaceholder.typicode.com/todos?_page=${pageParam}`
     );
-    const data = await res.json();
-    setTodos(data);
+    return res.json();
   };
 
-  /**
-   * Con el useEffect llamamos a
-   */
-  useEffect(() => {
-    fetchTodos();
-  }, []);
+  // Deestructuramos la respuesta del useInfiniteQuery:
+  const {
+    data,
+    status,
+    error,
+    fetchNextPage,
+    isFetchingNextPage,
+    hasNextPage,
+  } = useInfiniteQuery({
+    queryKey: ["todos"],
+    queryFn: fetchTodos,
+    initialPageParam: 1,
+    getNextPageParam: (lastPage, allPages) => {
+      console.log({ lastPage, allPages });
+      // Con allPages obtiene las páginas cargadas (si se presiona click serán +1)
+      // El método length devuelve el valor como tal.
+      // return 20 + 1;
+      // Truco para que ya no devuelva más valores si se ha excedido el número de páginas:
+      const nextPage = lastPage.length ? allPages.length + 1 : undefined;
+      return nextPage;
+    },
+  });
+
+  console.log("data", data);
+
+  const content = data?.pages.map((todos: todo[]) =>
+    todos.map((todo) => {
+      return <TodoCard key={todo.id} todo={todo} />;
+    })
+  );
+
+  if (status === "pending") {
+    return <p>Loading...</p>;
+  }
+
+  if (status === "error") {
+    return <p>Error: {error.message}</p>;
+  }
 
   return (
     <div className="app">
-      {todos.map((todo) => (
+      {content}
+      {/* Si el hasNextPage es falso, ya no debería mostrarse el botón */}
+      <button
+        disabled={!hasNextPage || isFetchingNextPage}
+        onClick={() => fetchNextPage()}
+      >
+        {isFetchingNextPage
+          ? "Loading more ..."
+          : hasNextPage
+          ? "Load more"
+          : "Nothing more to load"}
+      </button>
+      {/* {todos.map((todo) => (
         <TodoCard key={todo.id} todo={todo} />
-      ))}
+      ))} */}
     </div>
   );
 }
